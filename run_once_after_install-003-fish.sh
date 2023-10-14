9f5ecf8e9085f0c718f3a8f3f34c36118f12844c
Bram Wubs <git@wubscloud.nl>
2023-10-14 15:10:08 +0200

    Update install-003-fish.sh Add install-004-fisher.fish

--- run_once_after_install-003-fish.sh
+++ run_once_after_install-003-fish.sh
@@ -4,6 +4,3 @@ sudo echo "/opt/homebrew/bin/fish"  >> /etc/shells
 
 chsh -s /opt/homebrew/bin/fish
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-fisher install IlanCosman/tide@v6
---
+++ run_once_after_install-004-fisher.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+fisher install IlanCosman/tide@v6
