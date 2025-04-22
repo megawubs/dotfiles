b842cef87c648dd06d53238604f2724a52801e5d
Bram Wubs <github@wubscloud.nl>
2025-04-22 23:45:07 +0200

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,3 +17,17 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/bramwubs/miniconda3/bin/conda
+    eval /Users/bramwubs/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/bramwubs/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/bramwubs/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/bramwubs/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
