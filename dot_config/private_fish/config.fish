d9630edb528c17ab3435402d5c507a28954d82f4
Bram Wubs <github@wubscloud.nl>
2026-02-03 10:00:35 +0100

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_key_bindings
+
+    # Daily PDE tip (only once per day)
+    pde-tip --auto
 end
 
 set -gx EDITOR nvim
